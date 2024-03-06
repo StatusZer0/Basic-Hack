bdb956e3fec6e67625ddabc7e7a1bceae4fcb0f6
Zer0 <statuszero12@gmail.com>
2024-03-06 12:53:49 -0500

    Update main.cpp
    add a basic window that shows an FLAlertLayer

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <imgui-cocos.hpp>
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
 
 $on_mod(Loaded) {
     ImGuiCocos::get().setup([] {
-        ImGui::ShowDemoWindow();
+        ImGui::Begin("Basic Hack");
+
+		if (ImGui::Button("Test Alert"))
+		{
+			FLAlertLayer::create(
+				"Basic Hack", //title
+				"Let's hope this works.", //content
+				"It worked" // button
+			)->show();
+		}
     });
 }
