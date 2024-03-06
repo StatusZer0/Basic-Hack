b4494581b9d1bcd0481caff9fc25cbff94b3ccb9
Zer0 <statuszero12@gmail.com>
2024-03-06 11:06:38 -0500

    Update main.cpp
    add the ImGui demo module for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,15 @@
-#include <Geode/Geode.hpp>
+#include <imgui-cocos.hpp>
 
-using namespace geode::prelude;
+$on_mod(Loaded) {
+    ImGuiCocos::get().setup([] {
+        // this runs after imgui has been setup,
+        // its a callback as imgui will be re initialized when toggling fullscreen,
+        // so use this to setup any themes and or fonts!
+    }).draw([] {
+        ImGui::Begin("My awesome window");
 
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) {
-			return false;
-		}
-
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			menu_selector(MyMenuLayer::onMyButton)
-		);
-
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		myButton->setID("my-button"_spr);
-
-		menu->updateLayout();
-
-		return true;
-	}
-
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
-};
+        ImGui::Button("Awesome button");
+        
+        ImGui::End();
+    });
+}
