ac31ed001dfd9e47e271b8466a1b318ea223c53c
Zer0 <statuszero12@gmail.com>
2024-03-06 13:03:34 -0500

    Removed ImGui
    need to make it mobile compatible, so I'm moving to Geode UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,8 @@
-#include <imgui-cocos.hpp>
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
-$on_mod(Loaded) {
-    ImGuiCocos::get().setup([] {}).draw([] {
-        ImGui::Begin("Basic Hack");
-
-		if (ImGui::Button("Test Alert"))
-		{
-			FLAlertLayer::create(
-				"Basic Hack", //title
-				"Let's hope this works.", //content
-				"It worked" // button
-			)->show();
-		}
-
-		ImGui::End();
-    });
+class BasicHackMenu
+{
+	
 }
