b8cbe743d64ed78ace72f6d68260973952ff3efa
Zer0 <statuszero12@gmail.com>
2024-03-06 12:41:53 -0500

    ImGui Demo Window
    edit CMakeLists.txt to add all files in "src" and "src\hacks".
    
    edit "main.cpp" to show the ImGui demo window

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,7 @@
 #include <imgui-cocos.hpp>
 
 $on_mod(Loaded) {
-    ImGuiCocos::get().setup([] {
-        // this runs after imgui has been setup,
-        // its a callback as imgui will be re initialized when toggling fullscreen,
-        // so use this to setup any themes and or fonts!
-    }).draw([] {
-        ImGui::Begin("My awesome window");
+    ImGuiCocos::get().setup([] {});
 
-        ImGui::Button("Awesome button");
-        
-        ImGui::End();
-    });
+	ImGui::ShowDemoWindow();
 }
