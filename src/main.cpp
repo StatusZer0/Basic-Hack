efe070e735037a6ef0955d5609f614b7dba4138e
Zer0 <statuszero12@gmail.com>
2024-03-06 13:04:32 -0500

    Revert "Removed ImGui"
    This reverts commit ac31ed001dfd9e47e271b8466a1b318ea223c53c.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
+#include <imgui-cocos.hpp>
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
-class BasicHackMenu
-{
-	
+$on_mod(Loaded) {
+    ImGuiCocos::get().setup([] {}).draw([] {
+        ImGui::Begin("Basic Hack");
+
+		if (ImGui::Button("Test Alert"))
+		{
+			FLAlertLayer::create(
+				"Basic Hack", //title
+				"Let's hope this works.", //content
+				"It worked" // button
+			)->show();
+		}
+
+		ImGui::End();
+    });
 }
